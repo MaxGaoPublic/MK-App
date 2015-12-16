345389b165ebc95a7cf472504e45292b12c01b8b
BuildTools <max.gao@icloud.com>
2015-12-16 17:45:05 +0100

    Initial Commit

---
+++ MK-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MK-App
+//
+//  Created by Max Gao on 16.12.15.
+//  Copyright © 2015 Max Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
